187afa46a9437b168569c84e44543011f6776fea
Melamoto <Melamoto@gmail.com>
2016-02-10 01:14:05 +0000

    Fix sample, add network building
    Functionality is still incomplete - the connection from the output layer
    to the input layer should output the target values instead of the actual
    output values during training, to provide correct context.
    Also, the output layer includes plan nodes in order to make the input
    and output dimensions match; it shouldn't have any real negative impact,
    but it's unnecessary and a bit messy.

--- methods/todd-ann.py
+++ methods/todd-ann.py
@@ -4,6 +4,9 @@ ANN based on Todd's design
 """
 
 from pybrain.structure.connections.connection import Connection
+from pybrain.structure import LinearLayer, SigmoidLayer
+from pybrain.structure import RecurrentNetwork
+from pybrain.structure import FullConnection
 from pybrain.datasets.sequential import SequentialDataSet
 from scipy import dot
 
@@ -22,10 +25,10 @@ class WeightedPartialIdentityConnection(Connection):
         self.maxIndex = maxIndex
 
     def _forwardImplementation(self, inbuf, outbuf):
-        outbuf += dot([i*self.weight for i in inbuf],[1]*self.maxIndex + [0]*self.maxIndex)
+        outbuf += dot([i*self.weight for i in inbuf],[1]*self.maxIndex + [0]*(self.indim-self.maxIndex))
 
     def _backwardImplementation(self, outerr, inerr, inbuf):
-        inerr += dot([i*self.weight for i in outerr],[1]*self.maxIndex + [0]*self.maxIndex)
+        inerr += dot([i*self.weight for i in outerr],[1]*self.maxIndex + [0]*(self.indim-self.maxIndex))
 
 pitchCount = 12
 planCount = 0
@@ -34,7 +37,7 @@ def sampleSize():
     return pitchCount + planCount + 1
     
 def outputSize():
-    return pitchCount + 1
+    return sampleSize() #pitchCount + 1
 
 def makeNoteSample(pitch, newNote, plan):
     sample = [0] * sampleSize()
@@ -51,6 +54,17 @@ def makeNoteSample(pitch, newNote, plan):
         else:
             sample[i+pitchCount+1] = 0
     return sample
+    
+def makeNoteTarget(pitch, newNote):
+    target = [0] * outputSize()
+    for i in range(pitchCount):
+        if i == pitch:
+            target[i] = 1
+        else:
+            target[i] = 0
+    target[pitchCount] = newNote
+    #target = target + [0]*planCount
+    return target
 
 class Melody():
     def __init__(self,plan):
@@ -59,8 +73,9 @@ class Melody():
         self.plan = plan
 
     def addSamples(self, dataSet):
-        for s in range(len(self.pitches)):
-            dataSet.addSample(makeNoteSample(self.pitches[s], self.newNotes[s], self.plan))
+        for s in range(len(self.pitches)-1):
+            dataSet.addSample(makeNoteSample(self.pitches[s], self.newNotes[s], self.plan),
+                              makeNoteTarget(self.pitches[s+1], self.newNotes[s+1]))
 
     def addNote(self, pitch, newNote):
         self.pitches.append(pitch)
@@ -71,6 +86,25 @@ def makeMelodyDataSet(melodies):
     for m in melodies:
         seqDataSet.newSequence()
         m.addSamples(seqDataSet)
-    
-    
+    return seqDataSet
+
+def buildToddNetwork():
+    net = RecurrentNetwork()
+    inLayer = LinearLayer(sampleSize())
+    hiddenLayer = SigmoidLayer(6)
+    outLayer = SigmoidLayer(outputSize())
+    net.addInputModule(inLayer)
+    net.addModule(hiddenLayer)
+    net.addOutputModule(outLayer)
+    inRecursive = WeightedPartialIdentityConnection(0.8, pitchCount, inLayer, inLayer)
+    inToHidden = FullConnection(inLayer, hiddenLayer)
+    hiddenToOut = FullConnection(hiddenLayer, outLayer)
+    feedbackRecursive = WeightedPartialIdentityConnection(1, pitchCount, outLayer, inLayer)
+    net.addRecurrentConnection(inRecursive)
+    net.addConnection(inToHidden)
+    net.addConnection(hiddenToOut)
+    net.addRecurrentConnection(feedbackRecursive)
+    net.sortModules()
+    return net
+
 
